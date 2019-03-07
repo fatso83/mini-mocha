ffc774289610bce68f8f46c8a0e31b7c2f50f622
Carl-Erik Kopseng <carlerik@gmail.com>
2019-03-07 10:35:20 +0100

    API change: setup() and explicit exports

--- demo.js
+++ demo.js
@@ -1,30 +1,32 @@
-require('./index');
+require("./index").install();
 
 describe("Block A", function() {
-    var Foo = {
-        myMethod: function() { return "original"; }
+  var Foo = {
+    myMethod: function() {
+      return "original";
     }
+  };
 
-    before(() => console.log("before"));
-    after(() => console.log("after"));
-    beforeEach(() => console.log("before each"));
-    afterEach(() => console.log("after each"));
+  before(() => console.log("before"));
+  after(() => console.log("after"));
+  beforeEach(() => console.log("before each"));
+  afterEach(() => console.log("after each"));
 
-    it("normal sync test", function() {
-        // no-op
-    });
+  it("normal sync test", function() {
+    // no-op
+  });
 
-    it("failing test", function() {
-        throw new Error("My error");
-    });
+  it("failing test", function() {
+    throw new Error("My error");
+  });
 
-    it("async test (not supported)", function(done) {
-        console.warn("should never get here");
-    });
+  it("async test (not supported)", function(done) {
+    console.warn("should never get here");
+  });
 });
 
 describe("Block B", function() {
-    it("failing test", function() {
-        throw new Error("My error");
-    });
+  it("failing test", function() {
+    throw new Error("My error");
+  });
 });
--- index.js
+++ index.js
@@ -1,58 +1,74 @@
 var d = "";
 var its = [];
-function it(title, fn) { 
-    its.push(function() {
-        if(fn.length) return unsupported("async/callback testing")();
+function it(title, fn) {
+  its.push(function() {
+    if (fn.length) return unsupported("async/callback testing")();
 
-        beforeEachs.forEach(run);
-        try{ fn(); log(title, null); } 
-        catch(err){ log(title, err.message); } 
-        afterEachs.forEach(run);
-    });
-}
-function describe(title, fn) { 
-    d = title;
-    fn();
-    befores.forEach(run)
-    its.forEach(run)
-    afters.forEach(run)
-    its = [];
-    befores = [];
-    afters = [];
-}
-function log(title, err) { 
-    var status = err? `❌. ${err}`: "✔️";
-    console.log(d && `${d}: ` + `${title}: ${status}`);
-}
-function run(fn){ fn(); }
-function unsupported(title) {
-    return function(){
-        console.error("This operation is unsupported: " + title)
+    beforeEachs.forEach(run);
+    try {
+      fn();
+      log(title, null);
+    } catch (err) {
+      log(title, err.message);
     }
+    afterEachs.forEach(run);
+  });
+}
+function describe(title, fn) {
+  d = title;
+  fn();
+  befores.forEach(run);
+  its.forEach(run);
+  afters.forEach(run);
+  its = [];
+  befores = [];
+  afters = [];
+}
+function log(title, err) {
+  var status = err ? `❌. ${err}` : "✔️";
+  console.log(d && `${d}: ` + `${title}: ${status}`);
+}
+function run(fn) {
+  fn();
+}
+function unsupported(title) {
+  return function() {
+    console.error("This operation is unsupported: " + title);
+  };
 }
 
 var befores = [];
-function before(fn){
-    befores.push(fn);
+function before(fn) {
+  befores.push(fn);
 }
 var afters = [];
-function after(fn){
-    afters.push(fn);
+function after(fn) {
+  afters.push(fn);
 }
 var beforeEachs = [];
-function beforeEach(fn){
-    beforeEachs.push(fn);
+function beforeEach(fn) {
+  beforeEachs.push(fn);
 }
 var afterEachs = [];
-function afterEach(fn){
-    afterEachs.push(fn);
+function afterEach(fn) {
+  afterEachs.push(fn);
 }
 
-global.describe = describe;
-global.it = it;
-global.before = before;
-global.after = after;
-//global.beforeEach = unsupported("beforeEach");
-//global.afterEach =  unsupported("afterEach");
-global.beforeEach = beforeEach;
-global.afterEach =  afterEach;
+function install() {
+  global.describe = describe;
+  global.it = it;
+  global.before = before;
+  global.after = after;
+  global.beforeEach = beforeEach;
+  global.afterEach = afterEach;
+}
+
+module.exports = {
+  describe: describe,
+  it: it,
+  before: before,
+  after: after,
+  beforeEach: beforeEach,
+  afterEach: afterEach,
+  install
+};
