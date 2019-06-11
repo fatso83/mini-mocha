1c18c75ca6e8f9727feb9c529b55bcc006e0f51e
rpgeeganage <rpgeeg@gmail.com>
2019-06-11 20:09:18 +0200

    modified the functionality and extend the test with more complex one

--- test/test_case/test_5.js
+++ test/test_case/test_5.js
@@ -67,6 +67,58 @@ describe("main describe", () => {
         it("it 2 from inner describe 2", () => {
             console.log("it 2 from inner describe 2");
         });
+
+        describe("inner describe 2 deep describe 1", () => {
+            before(() => {
+                console.log("inner describe 2 deep describe 1 before");
+            });
+
+            beforeEach(() => {
+                console.log("inner describe 2 deep describe 1 beforeEach");
+            });
+
+            after(() => {
+                console.log("inner describe 2 deep describe 1 after");
+            });
+
+            afterEach(() => {
+                console.log("inner describe 2 deep describe 1 afterEach");
+            });
+
+            it("it 1 from inner describe 2 deep describe 1", () => {
+                console.log("it 1 from inner describe 2 deep describe 1");
+            });
+
+            it("it 2 from inner describe 2 deep describe 1", () => {
+                console.log("it 2 from inner describe 2 deep describe 1");
+            });
+        });
+
+        describe("inner describe 2 deep describe 2", () => {
+            before(() => {
+                console.log("inner describe 2 deep describe 1 before");
+            });
+
+            beforeEach(() => {
+                console.log("inner describe 2 deep describe 1 beforeEach");
+            });
+
+            after(() => {
+                console.log("inner describe 2 deep describe 1 after");
+            });
+
+            afterEach(() => {
+                console.log("inner describe 2 deep describe 1 afterEach");
+            });
+
+            it("it 1 from inner describe 2 deep describe 2", () => {
+                console.log("it 1 from inner describe 2 deep describe 2");
+            });
+
+            it("it 2 from inner describe 2 deep describe 2", () => {
+                console.log("it 2 from inner describe 2 deep describe 2");
+            });
+        });
     });
 
     it("main describe block standalone it", () => {
