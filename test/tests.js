352429da9edce9290c6cc91314794c7234f8bd28
rpgeeganage <rpgeeg@gmail.com>
2019-06-16 22:02:09 +0200

    fix errors

--- test/tests.js
+++ test/tests.js
@@ -1,51 +1,51 @@
-const assert = require('assert');
+const assert = require("assert");
 
-const { getSnapshotContent, runTest } = require('./helper');
+const { getSnapshotContent, runTest } = require("./helper");
 
-runTest('test_case/test_1.js', 'snapshot/test_1.txt', (err, op) => {
-  if (err) {
-    throw err;
-  }
+runTest("test_case/test_1.js", "snapshot/test_1.txt", (err, op) => {
+    if (err) {
+        throw err;
+    }
 
-  assert.strictEqual(op, getSnapshotContent('snapshot/test_1.txt'));
+    assert.strictEqual(op, getSnapshotContent("snapshot/test_1.txt"));
 });
 
-runTest('test_case/test_2.js', 'snapshot/test_2.txt', (err, op) => {
-  if (err) {
-    throw err;
-  }
+runTest("test_case/test_2.js", "snapshot/test_2.txt", (err, op) => {
+    if (err) {
+        throw err;
+    }
 
-  assert.strictEqual(op, getSnapshotContent('snapshot/test_2.txt'));
+    assert.strictEqual(op, getSnapshotContent("snapshot/test_2.txt"));
 });
 
-runTest('test_case/test_3.js', 'snapshot/test_3.txt', (err, op) => {
-  if (err) {
-    throw err;
-  }
+runTest("test_case/test_3.js", "snapshot/test_3.txt", (err, op) => {
+    if (err) {
+        throw err;
+    }
 
-  assert.strictEqual(op, getSnapshotContent('snapshot/test_3.txt'));
+    assert.strictEqual(op, getSnapshotContent("snapshot/test_3.txt"));
 });
 
-runTest('test_case/test_4.js', 'snapshot/test_4.txt', (err, op) => {
-  if (err) {
-    throw err;
-  }
+runTest("test_case/test_4.js", "snapshot/test_4.txt", (err, op) => {
+    if (err) {
+        throw err;
+    }
 
-  assert.strictEqual(op, getSnapshotContent('snapshot/test_4.txt'));
+    assert.strictEqual(op, getSnapshotContent("snapshot/test_4.txt"));
 });
 
-runTest('test_case/test_5.js', 'snapshot/test_5.txt', (err, op) => {
-  if (err) {
-    throw err;
-  }
+runTest("test_case/test_5.js", "snapshot/test_5.txt", (err, op) => {
+    if (err) {
+        throw err;
+    }
 
-  assert.strictEqual(op, getSnapshotContent('snapshot/test_5.txt'));
+    assert.strictEqual(op, getSnapshotContent("snapshot/test_5.txt"));
 });
 
-runTest('test_case/test_6.js', 'snapshot/test_6.txt', (err, op) => {
-  if (err) {
-    throw err;
-  }
-
-  assert.strictEqual(op, getSnapshotContent('snapshot/test_6.txt'));
+runTest("test_case/test_6.js", "snapshot/test_6.txt", (err, op) => {
+    if (err) {
+        throw err;
+    }
+    const regex = /[\n\t\s\+']/g;
+    assert.strictEqual(getSnapshotContent("snapshot/test_6.txt").replace(regex, ""), op.replace(regex, ""));
 });
