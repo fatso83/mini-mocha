14ef9410ec62ca7863e828896605f2a1d96739e6
Carl-Erik Kopseng <carlerik@gmail.com>
2022-01-30 10:27:40 +0100

    Add test of correct exit status
    refs #23

---
+++ integration-test/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
+should_exit_with_non_zero_on_failure(){
+    cat > failure_test.js << EOF
+require("..").install();
+test("this fails", () => {
+    throw new Error("something wrong");
+});
+EOF
+
+    node failure_test.js
+}
+
+should_have_exit_status_zero_on_failure(){
+    cat > pass_test.js << EOF
+require("..").install();
+test("this is ok", () => { });
+EOF
+
+    node pass_test.js
+}
+
+should_have_exit_status_zero_on_failure
+if [[ $? != 0 ]]; then
+    echo "It exited with non-zero status on a non-failing test!"
+    exit 1
+fi
+
+should_exit_with_non_zero_on_failure
+if [[ $? == 0 ]]; then
+    echo "It did not exit with non-zero status on failure!"
+    exit 1
+fi
